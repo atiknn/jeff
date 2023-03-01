a253111c493d8ad8ebd6bbebf540d0f8a8bbf5da
atikn <atiknaikwadi1982@gmail.com>
2023-03-01 10:07:11 +0000

    fmt

--- instance.tf
+++ instance.tf
@@ -1,12 +1,12 @@
 
 resource "aws_instance" "ec2" {
-    ami = "ami-005e54dee72cc1d00"
-    instance_type = "t2.micro"
-    associate_public_ip_address = true
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
-    subnet_id = aws_subnet.public_subnet[0]
+  ami                         = "ami-005e54dee72cc1d00"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  subnet_id                   = aws_subnet.public_subnet[0]
 
-    tags = {
+  tags = {
     Name = "${var.environment_code}_main_ec2_instance"
   }
 }
@@ -17,18 +17,18 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main_vpc.cidr_block]   
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
