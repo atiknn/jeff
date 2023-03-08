c636198ccd8fd529b3638eb21143ce6fb0869b01
atikn <atiknaikwadi1982@gmail.com>
2023-03-08 17:02:06 +0000

    security groups

--- instance.tf
+++ instance.tf
@@ -51,6 +51,16 @@ resource "aws_security_group" "allow_tls" {
 
   }
 
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
