49457693b6cea83c203becc9e94febb2a54c5c28
atikn <atiknaikwadi1982@gmail.com>
2023-03-08 16:31:30 +0000

    security group open to internet

--- instance.tf
+++ instance.tf
@@ -47,16 +47,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
-
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 
