cbaf5ed76765a6a8cc80844cea0bb3c870776f2c
atikn <atiknaikwadi1982@gmail.com>
2023-03-17 18:57:17 +0000

    fmt

--- level2/data.tf
+++ level2/data.tf
@@ -1,9 +1,9 @@
-data "terraform_remote_state" "level1"{
-    backend = "s3"
+data "terraform_remote_state" "level1" {
+  backend = "s3"
 
-    config = {
-        bucket = "terraformstatebucketl1"
-        key            = "level1.tfstate"
-        region = "us-east-1"
-    }
+  config = {
+    bucket = "terraformstatebucketl1"
+    key    = "level1.tfstate"
+    region = "us-east-1"
+  }
 }
