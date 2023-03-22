3b78ee1e21efa269d5696998521987a20c2e7cab
atikn <atiknaikwadi1982@gmail.com>
2023-03-22 16:30:40 +0000

    init automated state

---
+++ level0/main.tf
@@ -0,0 +1,17 @@
+
+resource "aws_s3_bucket" "s3_bucket_tf_state" {
+  bucket = "s3-bucket-tf-state"
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
--- level0/provider.tf
+++ level0/provider.tf
@@ -2,28 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "s3_bucket_tf_state" {
-  bucket = "s3-bucket-tf-state"
-}
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "s3-bucket-tf-state"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
+
+
+
+  required_version = ">= 1.2.0"
 }
+
