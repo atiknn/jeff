d5c1a3e1786d10fa0d096f5239685c2f6a428f7f
atikn <atiknaikwadi1982@gmail.com>
2023-03-22 17:04:51 +0000

    upgrade

--- level0/main.tf
+++ level0/main.tf
@@ -1,10 +1,10 @@
 
 resource "aws_s3_bucket" "s3_bucket_tf_state" {
-  bucket = "s3-bucket-tf-state"
+  bucket = "terraformstatebucketl1"
 }
 
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
+  name           = "terraformstatedynamo"
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20
