5cbc0e1a8bea0508d8ebb3a4942ee3a2aa1f348e
atiknn <atiknaikwadi1982@gmail.com>
2023-05-12 15:34:48 +0100

    attached s3 policy

--- level2/iam-new.tf
+++ level2/iam-new.tf
@@ -5,3 +5,12 @@ resource "aws_iam_role" "iam_role_autoscaling" {
     tag-key = "iam_role_autoscaling"
   }
 }
+
+data "aws_iam_policy" "aws_iam_policy" {
+  arn = "arn:aws:s3:::*"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment_s3" {
+  role       = aws_iam_role.iam_role_autoscaling.name
+  policy_arn = data.aws_iam_policy.aws_iam_policy.arn
+}
