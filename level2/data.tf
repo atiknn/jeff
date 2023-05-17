85370ef8825cc73cbb576060ce3e07204601c16b
atiknn <atiknaikwadi1982@gmail.com>
2023-05-17 15:54:06 +0100

    policy-role

--- level2/data.tf
+++ level2/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "level1" {
   backend = "s3"
 
   config = {
-    bucket = "terraformstatebucketl1"
+    bucket = "terraformstatebucketl2"
     key    = "level1.tfstate"
     region = "us-east-1"
   }
--- level2/iam-new.tf
+++ level2/iam-new.tf
@@ -1,16 +1,27 @@
 resource "aws_iam_role" "iam_role_autoscaling" {
   name = "iam_role_autoscaling"
 
+  assume_role_policy = <<EOF
+    {
+  "Id": "Policy1684334551610",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1684334545250",
+      "Action": "s3:*",
+      "Effect": "Allow",     
+      "Principal": {
+        "Service": [
+          "autoscaling.amazonaws.com"
+        ]
+      }
+    }
+  ]
+}
+EOF
+
   tags = {
     tag-key = "iam_role_autoscaling"
   }
 }
 
-data "aws_iam_policy" "aws_iam_policy" {
-  arn = "arn:aws:s3:::*"
-}
-
-resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment_s3" {
-  role       = aws_iam_role.iam_role_autoscaling.name
-  policy_arn = data.aws_iam_policy.aws_iam_policy.arn
-}
