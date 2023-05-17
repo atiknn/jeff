530af25f891ad3485c2a21955d953169c6d16c3e
atiknn <atiknaikwadi1982@gmail.com>
2023-05-17 16:19:32 +0100

    sts:assumepolicy

--- level2/iam-new.tf
+++ level2/iam-new.tf
@@ -2,21 +2,21 @@ resource "aws_iam_role" "iam_role_autoscaling" {
   name = "iam_role_autoscaling"
 
   assume_role_policy = <<EOF
-    {
-  "Id": "Policy1684334551610",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1684334545250",
-      "Action": "s3:*",
-      "Effect": "Allow",     
-      "Principal": {
-        "Service": [
-          "autoscaling.amazonaws.com"
-        ]
-      }
-    }
-  ]
+   {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "autoscaling.amazonaws.com"
+                ]
+            }
+        }
+    ]
 }
 EOF
 
