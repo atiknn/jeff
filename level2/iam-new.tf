e3fab3a09a92354c3678eb2b17333dea235b03e9
atiknn <atiknaikwadi1982@gmail.com>
2023-05-22 17:58:43 +0100

    changed policy 2

--- level2/iam-new.tf
+++ level2/iam-new.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role" "iam_role_autoscaling" {
             "Effect": "Allow",
             "Action": [
                 "sts:AssumeRole"
-            ],
+            ],           
             "Principal": {
                 "Service": [
                     "ec2.amazonaws.com"
@@ -25,3 +25,31 @@ EOF
   }
 }
 
+
+resource "aws_iam_policy" "s3policy" {
+  name        = "s3policy"
+  description = "s3policy"
+  policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": "s3:*",           
+              "Resource": "*"
+          }
+      ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role = aws_iam_role.iam_role_autoscaling.name
+  policy_arn = aws_iam_policy.s3policy.arn
+}
+
+
+resource "aws_iam_instance_profile" "aws_iam_instance_profile" {
+  name = "aws_iam_instance_profile"
+  role = aws_iam_role.iam_role_autoscaling.name
+}
