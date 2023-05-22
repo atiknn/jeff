e6266c84914692df92aa10f240bbbcdd4d962f2a
atiknn <atiknaikwadi1982@gmail.com>
2023-05-22 15:15:44 +0100

    changed policy

--- level2/iam-new.tf
+++ level2/iam-new.tf
@@ -3,16 +3,20 @@ resource "aws_iam_role" "iam_role_autoscaling" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Action": [
-        "s3:ListAllMyBuckets",
-        "s3:ListBucket"
-      ],
-  "Statement": {
-    "Effect": "Allow",
-    "Principal": {"Service": "ec2.amazonaws.com"},
-    "Action": "sts:AssumeRole"
-  }
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }
+    ]
 }
 EOF
 
